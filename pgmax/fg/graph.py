f985834da563af3cd8514a9dccfd37f2641f7ecd
Shrinu Kushagra <skushagra@ldp-skushagra.corp.vicarious.com>
2021-11-19 15:40:35 -0800

    adding docustrings to the public functions

--- pgmax/fg/graph.py
+++ pgmax/fg/graph.py
@@ -45,7 +45,8 @@ class FactorGraph:
     """
 
     variables: Union[
-        Mapping[Hashable, groups.VariableGroup],
+        Mapping[int, groups.VariableGroup],
+        Mapping[str, groups.VariableGroup],
         Sequence[groups.VariableGroup],
         groups.VariableGroup,
     ]
